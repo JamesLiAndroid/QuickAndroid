576fdc57bcbf8b88a5ca62ef82fd603f493559fb
lsy-android <lsy1234567@163.com>
2016-11-24 21:25:26 +0800

    最终提交，解决ViewPager和Fragment相互嵌套的问题！

--- app/src/main/java/com/james/li/quickandroid/fragment/HomeFragment.java
+++ app/src/main/java/com/james/li/quickandroid/fragment/HomeFragment.java
@@ -2,24 +2,33 @@ package com.james.li.quickandroid.fragment;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
 import com.james.li.quickandroid.Base.LazyFragment;
 import com.james.li.quickandroid.R;
-import java.util.Random;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by lsy-android on 10/16/16 in zsl-tech.
  */
 public class HomeFragment extends LazyFragment {
     private TextView tvLoading;
-    private ImageView ivContent;
+    private RelativeLayout rlContent;
+
+    private TabLayout tabLayout;
+    private ViewPager viewPagerP;
+
     private int tabIndex;
     public static final String INTENT_INT_INDEX="index";
 
-    private SwipeRefreshLayout swipeRefreshLayout;
+    private List<LazyFragment> list = new ArrayList<>();
+    private MyFragmentPagerAdapter adapter;
 
     public static HomeFragment newInstance(int tabIndex,boolean isLazyLoad) {
         Bundle args = new Bundle();
@@ -32,51 +41,29 @@ public class HomeFragment extends LazyFragment {
     @Override
     protected void onCreateViewLazy(Bundle savedInstanceState) {
         super.onCreateViewLazy(savedInstanceState);
-        setContentView(R.layout.fragment_tabmain_item);
+        setContentView(R.layout.fragment_home);
         tabIndex = getArguments().getInt(INTENT_INT_INDEX);
-        ivContent = (ImageView) findViewById(R.id.iv_content);
+        rlContent = (RelativeLayout) findViewById(R.id.rl_content);
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        viewPagerP = (ViewPager) findViewById(R.id.view_pager_p);
         tvLoading = (TextView) findViewById(R.id.tv_loading);
-        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
-        swipeRefreshLayout.setEnabled(true);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override public void onRefresh() {
-                refreshItems();
-            }
-        });
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+
+        MoreFragment fragment1 = MoreFragment.newInstance(1, false);
+        MoreFragment fragment2 = MoreFragment.newInstance(2, false);
+        MoreFragment fragment3 = MoreFragment.newInstance(3, false);
+
+        list.add(fragment1);
+        list.add(fragment2);
+        list.add(fragment3);
+
+        adapter = new MyFragmentPagerAdapter(getChildFragmentManager(), list);
+        viewPagerP.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(viewPagerP);
+        viewPagerP.setOffscreenPageLimit(list.size() - 1);
         getData();
-    }
-
-    private void refreshItems() {
-        int i = createRandom(1, 4);
-        int id = 0;
-        switch (i) {
-            case 1:
-                id=R.drawable.a;
-                break;
-            case 2:
-                id=R.drawable.b;
-                break;
-            case 3:
-                id=R.drawable.c;
-                break;
-            case 4:
-                id=R.drawable.d;
-                break;
-        }
-
-        ivContent.setImageResource(id);
-        stopRefresh();
-    }
-
-    private void stopRefresh() {
-        // Stop refresh animation
-        swipeRefreshLayout.setRefreshing(false);
-    }
-
-    private int createRandom(int min, int max) {
-        Random random = new Random();
-        int s = random.nextInt(max)  %  (max - min + 1) + min;
-        return s;
     }
 
     private void getData() {
@@ -100,23 +87,7 @@ public class HomeFragment extends LazyFragment {
     private Handler handler = new Handler() {
         public void handleMessage(android.os.Message msg) {
             tvLoading.setVisibility(View.GONE);
-            int id=0;
-            switch (tabIndex){
-                case 1:
-                    id=R.drawable.a;
-                    break;
-                case 2:
-                    id=R.drawable.b;
-                    break;
-                case 3:
-                    id=R.drawable.c;
-                    break;
-                case 4:
-                    id=R.drawable.d;
-                    break;
-            }
-            ivContent.setImageResource(id);
-            ivContent.setVisibility(View.VISIBLE);
+            rlContent.setVisibility(View.VISIBLE);
         }
     };
 }
--- app/src/main/java/com/james/li/quickandroid/fragment/MyFragmentPagerAdapter.java
+++ app/src/main/java/com/james/li/quickandroid/fragment/MyFragmentPagerAdapter.java
@@ -5,14 +5,13 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-import com.james.li.quickandroid.Base.BaseFragment;
-
 import com.james.li.quickandroid.Base.LazyFragment;
+
 import java.util.List;
 
 public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
     private List<LazyFragment> fragmentList;
-
+    private String titles[] = {"tab1", "tab2", "tab3"};
     public MyFragmentPagerAdapter(FragmentManager fm, List<LazyFragment> fragmentList) {
         super(fm);
         this.fragmentList = fragmentList;
@@ -33,4 +32,8 @@ public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
         return super.getItemPosition(object);
     }
 
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
 }
